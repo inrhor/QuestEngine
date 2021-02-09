e41d041dc0638ebc8cd5e176758291682789b407
inrhor <inrhor@aliyun.com>
2021-02-09 22:11:42 +0800

    v1.0.0.Beta1-undone - 耗时计算

--- src/main/kotlin/cn/inrhor/questengine/loader/PluginLoader.kt
+++ src/main/kotlin/cn/inrhor/questengine/loader/PluginLoader.kt
@@ -3,19 +3,24 @@ package cn.inrhor.questengine.loader
 import cn.inrhor.questengine.QuestEngine
 import cn.inrhor.questengine.common.dialog.DialogManager
 import cn.inrhor.questengine.common.item.ItemManager
+import cn.inrhor.questengine.utlis.public.MsgUtil
 import cn.inrhor.questengine.utlis.public.UseString
+import io.izzel.taboolib.module.locale.TLocale
 import org.bukkit.Bukkit
+import kotlin.system.measureTimeMillis
 
 class PluginLoader {
 
     fun init() {
         UpdateYaml().run(UseString.getLang())
         InfoSend().logoSend()
-
-        Bukkit.getScheduler().runTaskAsynchronously(QuestEngine.plugin, Runnable {
-            ItemManager().loadDialog()
-            DialogManager().loadDialog()
-        })
+            Bukkit.getScheduler().runTaskAsynchronously(QuestEngine.plugin, Runnable {
+                val timeCost = measureTimeMillis {
+                    ItemManager().loadDialog()
+                    DialogManager().loadDialog()
+                }
+                TLocale.sendToConsole("LOADER.TIME_COST", UseString.pluginTag, timeCost)
+            })
     }
 
 }
