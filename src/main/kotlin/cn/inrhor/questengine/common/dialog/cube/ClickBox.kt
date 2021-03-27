64f063a04d82b9cc1a55ce3fd6cf0ea0ba558402
inrhor <inrhor@aliyun.com>
2021-03-27 14:25:50 +0800

    v1.Alpha1-undone time-2021/3/27-1 放弃全息对话，封存到另一分支

--- src/main/kotlin/cn/inrhor/questengine/common/dialog/cube/ClickBox.kt
+++ src/main/kotlin/cn/inrhor/questengine/common/dialog/cube/ClickBox.kt
@@ -1,7 +1,5 @@
 package cn.inrhor.questengine.common.dialog.cube
 
-import cn.inrhor.questengine.common.database.data.DataStorage
-import cn.inrhor.questengine.utlis.public.MsgUtil
 import org.bukkit.Location
 import org.bukkit.entity.Player
 import org.bukkit.util.Vector
@@ -10,15 +8,13 @@ import kotlin.math.atan
 class ClickBox(val boxLoc: Location, val radius: Double) {
 
     fun isClick(player: Player): Boolean {
-        val playerData = DataStorage.playerDataStorage[player.uniqueId]!!
-        val clickBox = playerData.clickBoxMap["test"]!!
-
         val direction = Vector(0, 0, 0)
-        val distance: Double = player.eyeLocation.distance(clickBox.boxLoc)
-        direction.add(clickBox.boxLoc.toVector().subtract(player.eyeLocation.toVector()))
-        MsgUtil.send("HIT BOX")
+        val distance = player.eyeLocation.distance(this.boxLoc)
+        direction.add(this.boxLoc.toVector().subtract(player.eyeLocation.toVector()))
+        /*MsgUtil.send("HIT BOX")
         MsgUtil.send("click "+(direction.angle(player.location.direction) <= atan(radius / distance))) // 半径 / 距离
         MsgUtil.send("info  atan "+ atan(1 / distance) +"   dir "+(direction.angle(player.location.direction)))
+        */
         return (direction.angle(player.location.direction) <= atan(radius / distance))
     }
 
