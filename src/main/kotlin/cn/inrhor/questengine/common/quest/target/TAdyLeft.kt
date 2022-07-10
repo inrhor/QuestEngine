b2978752a85cf8685c13c330b300c498d2bed2dc
inrhor <inrhor@aliyun.com>
2022-07-10 21:24:28 +0800

    fix🐛:  修复目标Kether

---
+++ src/main/kotlin/cn/inrhor/questengine/common/quest/target/TAdyLeft.kt
@@ -0,0 +1,34 @@
+package cn.inrhor.questengine.common.quest.target
+
+import cn.inrhor.questengine.api.target.TargetExtend
+import cn.inrhor.questengine.api.target.util.TriggerUtils
+import cn.inrhor.questengine.common.database.data.doingTargets
+import net.citizensnpcs.api.event.NPCLeftClickEvent
+import org.bukkit.Bukkit
+import org.bukkit.entity.Player
+
+object TNpcLeft: TargetExtend<NPCLeftClickEvent>() {
+
+    override val name = "left npc"
+
+    init {
+        if (Bukkit.getPluginManager().getPlugin("Citizens") != null) {
+            event = NPCLeftClickEvent::class
+            tasker {
+                val player = clicker
+                match(player, npc.id.toString(), name)
+                player
+            }
+        }
+    }
+
+    fun match(player: Player, npcID: String, name: String) {
+        player.doingTargets(name).forEach {
+            val target = it.getTargetFrame()
+            if (TriggerUtils.idTrigger(target, npcID)) {
+                TriggerUtils.booleanTrigger(player, target)
+            }
+        }
+    }
+
+}
